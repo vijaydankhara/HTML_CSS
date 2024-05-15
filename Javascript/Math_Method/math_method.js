fa5b0e9374342f3b1c6365419b5784028cfb57c0
vijay dankhara <vijaydankhara111@gmail.com>
2024-05-15 10:09:49 +0530

    15-05-2024

---
+++ Javascript/Math_Method/math_method.js
@@ -0,0 +1,98 @@
+/** 
+Math.E               // returns Euler's number
+Math.PI              // returns PI
+Math.SQRT2           // returns the square root of 2
+Math.SQRT1_2         // returns the square root of 1/2
+Math.LN2             // returns the natural logarithm of 2
+Math.LN10            // returns the natural logarithm of 10
+Math.LOG2E           // returns base 2 logarithm of E
+Math.LOG10E          // returns base 10 logarithm of E
+*/
+
+
+// Math round ()    ==> returns the nearest integer.     
+// let ans = Math.round(2.5)        // 3
+// let ans = Math.round(2.2)        // 2
+// let ans = Math.round(2.7)        // 3
+// console.log(ans);
+
+// math celi()    ==> rounded up to its nearest integer.
+// let ans = Math.ceil(3.3);       // 4
+// let ans = Math.ceil(3.7);       // 4
+// let ans = Math.ceil(-3.7);         // -3
+// console.log(ans);
+
+// Math floor()   ==> rounded Down to its nearest integer.
+// let ans = Math.floor(4.8)     // 4
+// let ans = Math.floor(6.7)   // 6
+// console.log(ans);
+
+
+// Math trunc()  ==> return only integer value
+// let ans = Math.trunc(5.3);    // 5
+// let ans = Math.trunc(-5.8);      // -5
+// console.log(ans);
+
+// Math sign()  ==> return nagativ(-1) or positive(1) value
+// let ans = Math.sign(4)     //  1
+// let ans = Math.sign(-4)    // -1
+// let ans = Math.sign(0)        // 0
+// console.log(ans);
+
+// Math pow()  ==>  Math.pow(x, y) returns the value of x to the power of y.
+// let ans = Math.pow(2,2)     //4
+// let ans = Math.pow(2,3)     //8
+// let ans = Math.pow(8,2)     //64
+// console.log(ans);
+
+// Math sqrt()  // return the sqrt of value
+// let ans = Math.sqrt(16)         // 4
+// let ans = Math.sqrt(100)        // 10
+// console.log(ans);
+
+// Math abs()   ==>  returns the absolute (positive)
+// let ans = Math.abs(4) //4
+// let ans = Math.abs(-4) //4
+// console.log(ans);
+
+// Math min()  ==> find the longest number
+// let ans = Math.min(33,65,7,2,87,99);     // 2
+// let ans = Math.min(33,65,-7,2,87,99);       // -7
+// console.log(ans);
+
+// Math max()  ==>  return the largegt number
+// let ans = Math.max(33,65,7,2,87,99);            // 99
+// let ans = Math.max(33,65,-7,2,87,-99);            //  87
+// console.log(ans);
+
+
+// Math random()   ==> his return 0 to 1 rendom number and every time change the number
+// let ans = Math.random()     // 0.1081606716618948
+// console.log(ans);
+
+
+// Math sin()
+// let ans = Math.sin(90 * 3.14 / 180);   // 0.9999996829318346
+// let ans = Math.sin(30 * 3.14 / 180);   // 0.4997701026431024
+// console.log(ans);
+
+// Math cos()
+// let ans = Math.cos(0 * 3.14 / 180);  // 1
+// let ans = Math.cos(90 * 3.14 / 180);  // 0.0007963267107332633
+// console.log(ans);
+
+// Math log()
+// let ans = Math.log(2);        // 0.6931471805599453
+// let ans = Math.log(10);      // 2.302585092994046
+// console.log(ans);
+
+// Math log2()
+// let ans = Math.log2(8);    // 3
+// let ans = Math.log2(4);    // 2
+// let ans = Math.log2(10);    // 3.321928094887362
+// console.log(ans);
+
+// Math log10()
+// let ans = Math.log10(1000);     // 3
+// let ans = Math.log10(5345);     // 3.7279477095447966
+// console.log(ans);   
