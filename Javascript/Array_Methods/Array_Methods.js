1443abd00b82df078a57cfc0de6e5627c8f39171
vijay dankhara <vijaydankhara111@gmail.com>
2024-05-15 19:33:35 +0530

    15-05-2024

---
+++ Javascript/Array_Methods/Array_Methods.js
@@ -0,0 +1,14 @@
+/*
+Array Length
+ */
+
+// array length  ==> length of array
+// const car = ['audi','BMW','maruti'];
+// let ans = car.length;   // 3
+// console.log(ans);
+
+
+// Array push 
+const car = ['audi','BMW','maruti'];
+let ans = [car.push]='suzuki'
+console.log(car);
